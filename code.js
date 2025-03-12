dd30fcf816cb7b8b542944c893eda8fd8754365f
Aidan Newberry <91752566+regtoga@users.noreply.github.com>
2025-03-12 14:43:21 -0600

    I added a convert to Matrix function

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-function convertToAdjList(adjMatrix) {
+function convertToAdjListog(adjMatrix) {
     //make a new adjacency list the same length as the matrix
     const adjlist = new Array(adjMatrix.length);
     //for each row
@@ -18,3 +18,40 @@ function convertToAdjList(adjMatrix) {
 
     return adjlist;
 }
+
+function convertToAdjMatrix(adjList) {
+    const adjMatrix = [];
+
+    //for each row: (same number in both list and matrix)
+    for (let i = 0; i < adjList.length; i++) {
+        adjMatrix[i] = [];
+        //for each cell in the column set it to zero as a default value
+        for (let j = 0; j < adjList.length; j++) {
+            adjMatrix[i][j] = 0;
+        }
+        // for each element in the adj. list there must be atleast one change in the matrix
+        for (let j = 0; j < adjList[i].length; j++){
+            adjMatrix[i][adjList[i][j]] = 1;
+        }
+    }
+
+    return adjMatrix;
+}
+
+
+function convertToAdjList(adjMatrix) {
+    //by converting back and forth i am utilizing the single test to prove both functions true
+
+    //console.log(adjMatrix);
+    var step1 = convertToAdjListog(adjMatrix);
+    //console.log(step1);
+    var step2 = convertToAdjMatrix(step1);
+    //console.log(step2);
+    var step3 = convertToAdjListog(step2);
+    //console.log(step3);
+
+    return step3;
+}
+
+//convertToAdjList([[1,0],[0,1]])
+//console.log()
